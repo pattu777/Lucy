7e9b8e1a3f168dd8dd1b4b83d76b265e0afa3b16
Chinmaya <patanaikchinmaya@gmail.com>
2016-01-17 22:41:09 +0100

    4space indent

--- setup.py
+++ setup.py
@@ -3,22 +3,22 @@ from setuptools import setup, find_packages
 version = '1.0'
 
 setup(
-	name='lucy',
-	version=version,
-	description='Lucy generates a LICENSE file for your project from the command line for you.',
-	author='Chinmaya Kr. Patanaik',
-	author_email='patanaikchinmaya@gmail.com',
-	license='MIT',
-	keywords=['license', 'github', 'command line', 'cli'],
+    name='lucy',
+    version=version,
+    description='Lucy generates a LICENSE file for your project from the command line for you.',
+    author='Chinmaya Kr. Patanaik',
+    author_email='patanaikchinmaya@gmail.com',
+    license='MIT',
+    keywords=['license', 'github', 'command line', 'cli'],
     url='https://github.com/pattu777/Lucy',
-	packages=find_packages(),
-	install_requires=[
-		'Click',
-		'requests',
-	],
-	entry_points={
-		'console_scripts': [
-			'lucy=lucy.lucy:main'
-		],
-	}
+    packages=find_packages(),
+    install_requires=[
+        'Click',
+	'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'lucy=lucy.lucy:main'
+        ],
+    }
 )
